7e79bab11e3949e4aa3253dda121f5f0c5c1134d
Stefan Vlad <h5_em_1@yahoo.com>
2016-03-29 22:26:49 +0300

    English in source code and the required assignment in partitioning.py

--- python/fazan/fazan.py
+++ python/fazan/fazan.py
@@ -4,48 +4,48 @@ import os
 
 class Player(object):
 
-    def __init__(self, nume, win_turn=True, raspuns="", fazan="", scor=1):
+    def __init__(self, name, win_turn=True, answer="", fazan="", score=1):
 
-        self.nume = nume
+        self.name = name
         self.fazan = fazan
-        self.raspuns = raspuns
+        self.answer = answer
         self.win_turn = win_turn
-        self.scor = scor
+        self.score = score
 
     def my_turn(self):
 
-        self.raspuns = raw_input("Raspunsul tau: ")
-        return self.raspuns
+        self.answer = raw_input("raspunsul tau: ")
+        return self.answer
 
     def add_faz(self):
 
-        if self.scor == 1:
+        if self.score == 1:
             self.fazan = self.fazan + 'f'
 
-        elif self.scor == 2:
+        elif self.score == 2:
             self.fazan = self.fazan + 'a'
 
-        elif self.scor == 3:
+        elif self.score == 3:
             self.fazan = self.fazan + 'z'
 
-        elif self.scor == 4:
+        elif self.score == 4:
             self.fazan = self.fazan + 'a'
 
-        elif self.scor == 5:
+        elif self.score == 5:
             self.fazan = self.fazan + 'n'
-        self.scor += 1
+        self.score += 1
 
 
-class Calculator(Player):
+class Computer(Player):
 
     def __init__(self):
-        super(Calculator, self).__init__("Calculator")
+        super(Computer, self).__init__("Calculator")
 
     def my_turn(self, pref):
         global word_list
-        cuv = random.choice(word_list[pref])
-        word_list[pref].remove(cuv)
-        return cuv
+        random_word = random.choice(word_list[pref])
+        word_list[pref].remove(random_word)
+        return random_word
 
 files_in_dir = os.listdir('.')  # a list containing the names of the entries in the current directory
 word_list = {}  # a dictionary containing the words categorized by their prefixes
@@ -62,16 +62,16 @@ def player_turn(pref, player):
         print "Prefixul pentru cuvantul tau este: ", pref
         i = 0
         while i < 3:
-            alegere = player.my_turn()
-            if alegere in word_list[pref]:
-                word_list[pref].remove(alegere)
+            choice = player.my_turn()
+            if choice in word_list[pref]:
+                word_list[pref].remove(choice)
                 break
             i += 1
         else:
             print "Ai esuat in a gasi un cuvant, ai fost inchs! "
             return False
         print "Cuvant acceptat! "
-        return alegere
+        return choice
     else:
         print "Nu exista cuvant cu acest prefix, ai fost inchis!"
         return False
@@ -80,32 +80,32 @@ def player_turn(pref, player):
 def computer_turn(pref, computer):
 
     if pref in word_list:
-        alegere = computer.my_turn(pref)
-        return alegere
+        choice = computer.my_turn(pref)
+        return choice
     print "Felicitari, m-ai inchis! "
     return False
 
-computer = Calculator()
-nume = raw_input("Numele tau: ")
-player = Player(nume)
+computer = Computer()
+name = raw_input("namele tau: ")
+player = Player(name)
 
 while(player.fazan != 'fazan' and computer.fazan != 'fazan'):
 
     random_key = random.choice(word_list.keys())
-    cuv_curent = random.choice(word_list[random_key])
+    current_word = random.choice(word_list[random_key])
     while True:
-        print 'Cuvantul curent:', cuv_curent
-        cuv_curent = player_turn(cuv_curent[len(cuv_curent)-2:len(cuv_curent)], player)  # -2 since there is no '\n' i have to send just the last 2 characters
-        if cuv_curent is not False:
-                cuv_curent = computer_turn(cuv_curent[len(cuv_curent)-2:len(cuv_curent)], computer)  # same thing as abobe, sending just 2 characters
-                if cuv_curent is False:
+        print 'Cuvantul curent:', current_word
+        current_word = player_turn(current_word[len(current_word)-2:len(current_word)], player)  # -2 since there is no '\n' i have to send just the last 2 characters
+        if current_word is not False:
+                current_word = computer_turn(current_word[len(current_word)-2:len(current_word)], computer)  # same thing as abobe, sending just 2 characters
+                if current_word is False:
                     computer.add_faz()
                     break
                 else:
-                    print "Raspunsul calculatorului: ", cuv_curent
+                    print "answerul calculatorului: ", current_word
         else:
             player.add_faz()
             break
 
-    print player.nume + ':', player.fazan
-    print computer.nume + ':', computer.fazan
+    print player.name + ':', player.fazan
+    print computer.name + ':', computer.fazan

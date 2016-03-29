ef19c12b29258641a02c616ddad8dca2d320cf34
Stefan Vlad <h5_em_1@yahoo.com>
2016-03-29 15:39:12 +0300

    edited the sources following pep8 instructions

--- python/fazan/partitionare.py
+++ python/fazan/partitionare.py
@@ -1,18 +1,18 @@
 import time
 
 t0 = time.time()
-cuvinte = open('cuvinte.txt','r+')
-#alfabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y'
+cuvinte = open('cuvinte.txt', 'r+')
+#alfabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', ' q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y'
 cuv = cuvinte.readline()
 pref = cuv[0:2]
 pref1 = ""
-f = open(pref+'.txt','w')
+f = open(pref+'.txt', 'w')
 f.write(cuv)
 for cuv in cuvinte:
     if pref == pref1:
         f.write(cuv)
     else:
-        f = open(pref1+'.txt','w')
+        f = open(pref1+'.txt', 'w')
         pref = pref1
         f.write(cuv)
 print time.time() - t0
